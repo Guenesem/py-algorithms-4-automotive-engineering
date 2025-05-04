f600f1bba1af62d294cfa13c48daff890f937fda
Guenesem <147604401+Guenesem@users.noreply.github.com>
2025-05-04 17:07:19 +0000

    x

--- 01_basic-python/my_file.py
+++ 01_basic-python/my_file.py
@@ -1,6 +1,6 @@
-.ipynb_checkpoints/
-/venv
-__pycache__
-.idea
-references.bib.bak
-*.slides.html
+#Es soll eine Loop erstellt werden, die Zahlen ausgibt, bis meine Variable erreicht ist.
+#Die Variable soll 10 sein.
+
+for x in range(0,11):
+    x += 1
+    print("meine Variable ist:", x)
